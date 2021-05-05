700926bd9b85cf88f364da13506f427c3754b69e
LucaQuacquarelli <lucaq82@libero.it>
2021-05-05 13:14:04 +0200

    Generati 16 numeri random

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// I numeri non possono essere duplicati.
+// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L'utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero "vietato" o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito.
+
+// FUNCTION
+function getRandomNumber(min, max) {
+    var randomNumber = Math.floor(Math.random() * (max - min) + min);
+    return randomNumber;
+}
+// /FUNCTION
+
+var bombs = [];
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+for (var i = 0; i < 16; i++) {
+    var randomBombs = getRandomNumber(1, 100);
+    bombs.push(randomBombs);
+} console.log(bombs);
