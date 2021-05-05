e7460a5f7ebc15294ecb204580bdb025ba15eda7
LucaQuacquarelli <lucaq82@libero.it>
2021-05-05 13:47:40 +0200

    Verifica elementi gia presenti

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// I numeri non possono essere duplicati.
 // In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L'utente non può inserire più volte lo stesso numero.
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all'utente un altro numero.
@@ -10,11 +9,27 @@ function getRandomNumber(min, max) {
     var randomNumber = Math.floor(Math.random() * (max - min) + min);
     return randomNumber;
 }
+
+function isInArray(element, array) {
+    //verifico tutti gli elementi dell'array 
+    for (var i = 0; i < array.length; i++) {
+        // verifico se un elemento è uguale ad un elemento presente nell'array 
+        if (element == array[i]) {
+            return true;
+        }
+    }
+    return false;
+}
 // /FUNCTION
 
+// Il computer deve generare 16 numeri casuali tra 1 e 100 e I numeri non possono essere duplicati.
 var bombs = [];
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-for (var i = 0; i < 16; i++) {
+// continuo il ciclo finchè la lunghezza dell'array (bombs) non raggiunge 16 elementi
+while (bombs.length < 16) {
+    // genero numeri casuali da 1 a 100...
     var randomBombs = getRandomNumber(1, 100);
-    bombs.push(randomBombs);
-} console.log(bombs);
+    //... ma li inserisco nell'array (bombs) solo dopo aver verificato che non siano gia presenti
+    if (!isInArray(randomBombs, bombs)) {
+        bombs.push(randomBombs);
+    }
+} console.log("BOMBE", bombs);
